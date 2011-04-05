91bb2ffda02293bea933863d8f67930458d52a69
tejaswi <teja.kgp@gmail.com>
2011-04-05 16:45:00 +0530

    test 4

--- src/FileRecordsReader.cpp
+++ src/FileRecordsReader.cpp
@@ -40,6 +40,28 @@ vector<	vector <string > > myRecords;
 vector< string> FileRecordsReader::getRecord(const  string& myRecord)
 {
 	vector <string > record;
-	record.push_back(myRecord);
+	string delim (",");
+	tokenize(myRecord, record ,delim );
 	return record;	
 }
+void FileRecordsReader::tokenize(const string& str,
+                      vector<string>& tokens,
+                      const string& delimiters = ",")
+{
+    // Skip delimiters at beginning.
+        string::size_type lastPos = str.find_first_not_of(delimiters, 0);
+            // Find first "non-delimiter".
+                string::size_type pos     = str.find_first_of(delimiters, lastPos);
+   
+                   while (string::npos != pos || string::npos != lastPos)
+                       {
+                               // Found a token, add it to the vector.
+                                       tokens.push_back(str.substr(lastPos, pos - lastPos));
+                                               // Skip delimiters.  Note the "not_of"
+                                                       lastPos = str.find_first_not_of(delimiters, pos);
+                                                           // Find next "non-delimiter"
+                                                                      pos = str.find_first_of(delimiters, lastPos);
+                   }
+ }
+
+
--- src/FileRecordsReader.hpp
+++ src/FileRecordsReader.hpp
@@ -8,11 +8,15 @@ class FileRecordsReader : public RecordsReader
 	public:
 		FileRecordsReader( string fileName , int numOfCol ) throw( exception ) ;
 		virtual vector < vector<string> > getRecords() ;
-		virtual vector< string > getRecord( const string& myRecord); 
+		virtual vector< string > getRecord(const string& myRecord); 
 		virtual ~FileRecordsReader();
 	private:
 		ifstream myStream;
 		int myCol;
+		void tokenize(const string& str,
+                       vector<string>& tokens,
+                       const string& delimiters );
+
 	
 };
 #endif
