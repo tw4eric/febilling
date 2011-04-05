d67c26495970b8f9cc723803bd542188ef5c035e
ashish1980sharma <a@c.com>
2011-04-05 13:44:48 +0530

    Rateplan lookup classs.

---
+++ src/RatePlanLookup.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class RatePlanLookup {
+
+    private:
+
+    public:
+        float LookupRatePlan(long aPhoneNumber , char *aCallId){ return 10.0;}
+};
+
