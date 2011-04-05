5e2dde88a00a0c086bde9a142d4579bd527431de
Prabhu Pritam <prabhupritam@gmail.com>
2011-04-05 15:28:33 +0530

    added by PPD

---
+++ src/RecordsConsolidator.cc
@@ -0,0 +1,18 @@
+#ifndef _RECORDS_CONSOLIDATOR_CC_
+#define _RECORDS_CONSOLIDATOR_CC_
+
+#include <string>
+#include <vector>
+
+#include "RawRecord.h"
+
+using namespace std;
+
+class RecordsConsolidator {
+	private :
+		vector<RawRecord> theRawRecords;
+	public : 
+		RecordsConsolidator(vector<RawRecord> rawRecords) : theRawRecords(rawRecords) {
+		}
+};
+#endif // _RECORDS_CONSOLIDATOR_CC_
