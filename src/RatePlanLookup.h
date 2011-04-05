affc0aea824ba6b13b6896aeaace1dd73e01bcf0
ashish1980sharma <a@c.com>
2011-04-05 14:19:03 +0530

    moved method to cpp

--- src/RatePlanLookup.h
+++ src/RatePlanLookup.h
@@ -9,6 +9,6 @@ class RatePlanLookup {
     private:
 
     public:
-        float LookupRatePlan(long aPhoneNumber , char *aCallId){ return 10.0;}
+        float LookupRatePlan(long aPhoneNumber , char *aCallId);
 };
 
