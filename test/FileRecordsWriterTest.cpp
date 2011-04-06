9835c30a025e2864a35c0164dc3b4928cab3745d
Ankur Sharma <euxyabd@incdcl92.(none)>
2011-04-06 09:04:36 +0530

    Completed

--- test/FileRecordsWriterTest.cpp
+++ test/FileRecordsWriterTest.cpp
@@ -16,7 +16,37 @@ TEST(FileRecordsWriter, fileProvidedIsValid) {
 EXPECT_NO_THROW({FileRecordsWriter recordWriter = FileRecordsWriter(string("test.txt"));});	
 };
 
+TEST(FileRecordsWriter, dataWrittenToFile4line) {
+	
+	FileRecordsWriter recordWriter = FileRecordsWriter(string("test.txt"));
+	vector<string> str1;
+	str1.push_back("callId1");
+	str1.push_back("phone");
+	str1.push_back("time");
+	str1.push_back("duration");
+	vector<string> str2;
+	str2.push_back("callId2");
+	str2.push_back("phone");
+	str2.push_back("time");
+	str2.push_back("duration");
+	vector<string> str3;
+	str3.push_back("callId3");
+	str3.push_back("phone");
+	str3.push_back("time");
+	str3.push_back("duration");
+	vector<string> str4;
+	str4.push_back("callId4");
+	str4.push_back("phone");
+	str4.push_back("time");
+	str4.push_back("duration");
+	vector< vector<string> > strList;
+	strList.push_back(str1);
+	strList.push_back(str2);
+	strList.push_back(str3);
+	strList.push_back(str4);
 
+	 EXPECT_EQ(true, recordWriter.WriteRecords(strList));	 
+};
 
 TEST(FileRecordsWriter, dataNotWrittenToFile4lineTryCatch) {
 	
